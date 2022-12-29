10fa08ba4fcd9a30ad4e518a502e563d86ea71c2
pasharad <pasharad2231@gmail.com>
2022-12-29 15:48:26 +0330

    update

--- q1.py
+++
@@ -1,6 +0,0 @@
-import re
-
-user_input = input('enter your string: ')
-pattern = input('enter pattern: ')
-ans = re.findall(pattern, user_input)
-print(f'{pattern} occured {len(ans)} in your string.')
