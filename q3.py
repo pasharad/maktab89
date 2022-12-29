c2abc6f185eab08ad4c7d4d17219f3ad5d805ed8
pasharad <pasharad2231@gmail.com>
2022-12-29 15:27:38 +0330

    soale 3

--- q3.py
+++ q3.py
@@ -1,3 +1,10 @@
 import re
 user_input = input('enter your string: ')
-pattern = input('enter your pattern: ')
+ans = re.sub(r'\s', '_', user_input)
+finall = ''
+for i in range(len(user_input)):
+    if user_input[i] == '_' and user_input[i] == ans[i]:
+        finall += ' '
+    else:
+        finall += ans[i]
+print(finall)
