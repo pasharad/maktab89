5aa8a9ab413a56faa7840021235557f96fa15776
pasharad <pasharad2231@gmail.com>
2023-01-30 23:42:09 +0330

    fit menu

---
+++ hw/metro/menu.py
@@ -0,0 +1,34 @@
+class Menu:
+    first_page_menu = [
+        'Register New User',
+        'Log in as User',
+        'Log in as Administrator',
+        'Exit']
+
+    login_menu = [
+        'Bank Account Management',
+        'Buy Ticket',
+        'Log out']
+
+    bank_account_menu = [
+        'Deposit',
+        'Withdraw',
+        'Show Balance',
+        'Go back...']
+
+    buy_ticket_menu = [
+        'Chargeable',
+        'Disposable(you can use it only once)',
+        'Date Expire',
+        'Show Ticket List']
+
+    admin_menu = [
+        'Make transport',
+        'Edit transport',
+        'logout'
+    ]
+
+
+# menu = Menu
+# for v in enumerate(menu.buy_ticket_menu):
+#     print(f'|{v[0]+1}. {v[1]}|')
