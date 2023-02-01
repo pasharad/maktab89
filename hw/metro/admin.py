731d92cfaabe5b6ed499ec21530dd297d58b36cb
pasharad <pasharad2231@gmail.com>
2023-02-01 05:13:47 +0330

    fit admin

---
+++ hw/metro/admin.py
@@ -0,0 +1,39 @@
+from user import *
+from ticket import *
+
+
+class Transportation:
+    def __init__(self, tr_id, origin, destination, cost):
+        self.tr_id = tr_id
+        self.origin = origin
+        self.destination = destination
+        self.cost = cost
+
+
+class Admin(User):
+    def __init__(self, fname, lname, nat_code, date_of_birth, password):
+        super().__init__(fname, lname, nat_code, date_of_birth, password)
+        self.transport = []
+
+    def make_transport(self, tr_id, origin, destination, cost):
+        new_transport = Transportation(tr_id, origin, destination, cost)
+        self.transport.append(new_transport)
+
+    @classmethod
+    def edit_transport(cls, tr_id, origin, destination, cost):
+        cls.tr_id = tr_id
+        cls.cost = cost
+        cls.origin = origin
+        cls.destination = destination
+
+    @staticmethod
+    def create_ticket(ticket_name):
+        if ticket_name == 'Chargeable':
+            new_card = ChargeableCard()
+            return new_card
+        elif ticket_name == 'Expiration':
+            new_card = ExpirationCard
+            return new_card
+        else:
+            new_card = DisposableTicket
+            return new_card
