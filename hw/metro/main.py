393b3fc2f4ead0493d373154c6b5fbe252ad7454
pasharad <pasharad2231@gmail.com>
2023-02-01 04:11:25 +0330

    bank account option activated

--- hw/metro/main.py
+++ hw/metro/main.py
@@ -32,7 +32,8 @@ def run():
                     pickle.dump(new_user, user)
                 clear()
                 print('---- welcome ----')
-                input(f'You registered successful\nyour id is: {new_user.user_id}')
+                input(f'You registered successful\nyour id is: {new_user.user_id}\nyour bank account ID: '
+                      f'{new_user.bank_account.bank_account_id}')
             except AssertionError as e:
                 input(e)
         elif user_input == 2:
@@ -69,7 +70,7 @@ def run():
             else:
                 try:
                     with open(f'Users/{user_id}.pickle', 'rb') as user:
-                        logged_user = pickle.load(user)
+                        logged_user: User = pickle.load(user)
                     while 1:
                         clear()
                         print(f'---- {logged_user.fname} {logged_user.lname} ----')
@@ -77,9 +78,54 @@ def run():
                             print(f'|{v[0] + 1}. {v[1]}|')
                         lg_input = int(input('Enter your desire: '))
                         if lg_input == 1:
-                            pass
+                            clear()
+                            print('---- Bank Account Management ----')
+                            for v in enumerate(Menu.bank_account_menu):
+                                print(f'|{v[0] + 1}. {v[1]}|')
+                            bank_input = int(input('Enter your desire: '))
+                            if bank_input == 1:
+                                clear()
+                                print('---- Deposit ----')
+                                bank_id = int(input('Enter your bank ID: '))
+                                if logged_user.bank_account.bank_account_id == bank_id:
+                                    amount = int(input('Enter amount: '))
+                                    logged_user.bank_account.deposit(amount)
+                                else:
+                                    input('Bank ID was wrong!')
+                            elif bank_input == 2:
+                                clear()
+                                print('---- Withdraw ----')
+                                bank_id = int(input('Enter your bank ID: '))
+                                if logged_user.bank_account.bank_account_id == bank_id:
+                                    amount = int(input('Enter amount: '))
+                                    logged_user.bank_account.withdraw(amount)
+                                else:
+                                    input('Bank ID was wrong!')
+                            elif bank_input == 3:
+                                clear()
+                                print('---- Balance ----')
+                                bank_id = int(input('Enter your bank ID: '))
+                                if logged_user.bank_account.bank_account_id == bank_id:
+                                    input(logged_user.bank_account.show_balance(
+                                        f'{logged_user.fname} {logged_user.lname}'))
+                                else:
+                                    input('Bank ID was wrong!')
+                            else:
+                                break
                         elif lg_input == 2:
-                            pass
+                            clear()
+                            print('---- Buy Ticket ----')
+                            for v in enumerate(Menu.buy_ticket_menu):
+                                print(f'|{v[0] + 1}. {v[1]}|')
+                            buy_input = int(input('Enter your desire: '))
+                            if buy_input == 1:
+                                pass
+                            elif buy_input == 2:
+                                pass
+                            elif buy_input == 3:
+                                pass
+                            else:
+                                break
                         elif lg_input == 3:
                             pass
                         elif lg_input == 4:
@@ -87,7 +133,9 @@ def run():
                         elif lg_input == 5:
                             pass
                         elif lg_input == 6:
-                            pass
+                            with open(f'Users/{logged_user.user_id}.pickle', 'wb') as user:
+                                pickle.dump(logged_user, user)
+                            break
 
                 except FileNotFoundError:
                     input('User Not Found!')
@@ -103,3 +151,5 @@ if __name__ == '__main__':
 
 
 # 403b611d-da8b-4376-aa64-a10be8ae31fe
+# 4531473070
+# 4c7ef1c3-0f37-441e-907d-35ed85bed37d
